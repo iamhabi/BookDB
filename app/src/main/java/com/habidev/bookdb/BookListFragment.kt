161adbe3b6bf74ac5492322a3ebbdd09461dd5e1
iamhabi <habiam@pm.me>
2022-11-12 19:31:19 +0900

    add bottom navigation

--- app/src/main/java/com/habidev/bookdb/BookListFragment.kt
+++ app/src/main/java/com/habidev/bookdb/BookListFragment.kt
@@ -1,15 +1,22 @@
 package com.habidev.bookdb
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.room.Room
+import com.habidev.bookdb.database.BookDao
+import com.habidev.bookdb.database.BookDatabase
 import com.habidev.bookdb.databinding.BookListBinding
 
 class BookListFragment: Fragment() {
     private lateinit var viewBinding: BookListBinding
     private lateinit var bookViewModel: BookViewModel
+
+    private lateinit var bookDao: BookDao
+
     private lateinit var items: List<BookItem>
 
     override fun onCreateView(
@@ -20,16 +27,29 @@ class BookListFragment: Fragment() {
         viewBinding = BookListBinding.inflate(inflater, container, false)
         bookViewModel = BookViewModel()
 
-        items = bookViewModel.bookItemList
+//        initBookDao()
+
+//        items = bookDao.getAll()
 
         return viewBinding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onResume() {
+        super.onResume()
 
-        val adapter = BookAdapter(requireContext(), items)
+//        items = bookViewModel.bookItemList
 
-        viewBinding.bookRecyclerView.adapter = adapter
+//        val adapter = BookAdapter(requireContext(), items)
+
+//        viewBinding.bookRecyclerView.adapter = adapter
+    }
+
+    private fun initBookDao() {
+        val database = Room.databaseBuilder(
+            requireContext(),
+            BookDatabase::class.java, "books"
+        ).build()
+
+        bookDao = database.bookDao()
     }
 }
--- app/src/main/java/com/habidev/bookdb/MainActivity.kt
+++ app/src/main/java/com/habidev/bookdb/MainActivity.kt
@@ -3,6 +3,7 @@ package com.habidev.bookdb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.habidev.bookdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +15,49 @@ class MainActivity : AppCompatActivity() {
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
+        initViewPager()
+        initBottomNav()
+    }
+
+    private fun initViewPager() {
         val adapter = ViewPagerAdapter(this)
 
         viewPager = viewBinding.viewPager
 
         viewPager.adapter = adapter
+
+        viewPager.setCurrentItem(1, false)
+
+        viewPager.registerOnPageChangeCallback(object: OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                when (position) {
+                    0 -> viewBinding.bottomNav.selectedItemId = R.id.btn_camera_frag
+                    1 -> viewBinding.bottomNav.selectedItemId = R.id.btn_book_list_frag
+                }
+            }
+        })
+    }
+
+    private fun initBottomNav() {
+        viewBinding.bottomNav.selectedItemId = R.id.btn_book_list_frag
+
+        viewBinding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.btn_camera_frag -> changeToCameraFragment()
+                R.id.btn_book_list_frag -> changeToBookListFragment()
+            }
+
+            true
+        }
+    }
+
+    private fun changeToCameraFragment() {
+        viewPager.setCurrentItem(0, true)
+    }
+
+    private fun changeToBookListFragment() {
+        viewPager.setCurrentItem(1, true)
     }
 }
