151f816b7885a56811b111bbe7f84680a6fe98d8
iamhabi <habiam@pm.me>
2023-02-05 13:07:24 +0900

    show internet search result to recycler view

--- app/src/main/java/com/habidev/bookdb/Fragment/SearchFragment.kt
+++ app/src/main/java/com/habidev/bookdb/Fragment/SearchFragment.kt
@@ -2,6 +2,9 @@ package com.habidev.bookdb.Fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,7 +16,16 @@ import com.habidev.bookdb.Activity.DetailActivity
 import com.habidev.bookdb.BookItem
 import com.habidev.bookdb.BookListAdapter
 import com.habidev.bookdb.BookViewModel
+import com.habidev.bookdb.DevKey
 import com.habidev.bookdb.databinding.SearchBinding
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class SearchFragment: Fragment() {
     private lateinit var viewBinding: SearchBinding
@@ -34,6 +46,7 @@ class SearchFragment: Fragment() {
         viewBinding = SearchBinding.inflate(inflater, container, false)
 
         // TODO
+        //  Request Focus to Edit Text
         //  show keyboard
 
         return viewBinding.root
@@ -61,15 +74,16 @@ class SearchFragment: Fragment() {
         viewBinding.btnSearch.setOnClickListener {
             // TODO
             //  hide keyboard
+            //  Remove Focus from Edit Text
 
-            val searchString: String = viewBinding.editTextSearch.text.toString()
+            val query: String = viewBinding.editTextSearch.text.toString()
 
-            if (searchString == "") {
+            if (query == "") {
                 Toast.makeText(requireContext(), "Input Search Query", Toast.LENGTH_SHORT).show()
             } else {
                 viewBinding.textViewDbResult.visibility = View.VISIBLE
 
-                bookViewModel.search(searchString).observe(requireActivity()) { resultList ->
+                bookViewModel.search(query).observe(requireActivity()) { resultList ->
                     resultList.let {
                         dbResultList.clear()
                         dbResultList.addAll(resultList)
@@ -77,7 +91,93 @@ class SearchFragment: Fragment() {
                         dbListAdapter.notifyItemRangeInserted(0, dbResultList.size)
                     }
                 }
+
+                getInfoFromNaverAndShow(query)
             }
+        }
+    }
+
+    private fun jsonArrayToBookItemList(resultJson: String): List<BookItem> {
+        val resultJsonArray = JSONObject(resultJson).getJSONArray("items")
+
+        Log.d("Result", resultJsonArray.toString())
+
+        val bookItemList = mutableListOf<BookItem>()
+
+        for (i in 0 until resultJsonArray.length()) {
+            val jsonObject = resultJsonArray.getJSONObject(i)
+
+            val isbn = jsonObject.get("isbn") as String
+            val link = jsonObject.get("link") as String
+            val title = jsonObject.get("title") as String
+            val author = jsonObject.get("author") as String
+            val imageUrl = jsonObject.get("image") as String
+            val description = jsonObject.get("description") as String
+
+            bookItemList.add(
+                BookItem(isbn.toLong(), link, title, author, imageUrl, description)
+            )
+        }
+
+        return bookItemList
+    }
+
+    private fun getInfoFromNaverAndShow(query: String) {
+        Thread {
+            try {
+                val con = connect("${DevKey.URL}$query")
+
+                con?.requestMethod = "GET"
+                con?.setRequestProperty("X-Naver-Client-Id", DevKey.CLIENT_ID)
+                con?.setRequestProperty("X-Naver-Client-Secret", DevKey.CLIENT_SECRET)
+
+                val responseCode = con!!.responseCode
+                val result = if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                    readBody(con.inputStream)
+                } else { // 에러 발생
+                    readBody(con.errorStream)
+                }
+
+                val bookItemList = jsonArrayToBookItemList(result)
+
+                internetResultList.clear()
+                internetResultList.addAll(bookItemList)
+
+                Handler(Looper.getMainLooper()).post {
+                    viewBinding.textViewInternetResult.visibility = View.VISIBLE
+
+                    internetListAdapter.notifyItemRangeInserted(0, internetResultList.size)
+                }
+            } catch (e: Exception) {
+                Log.e("FAIL", e.toString())
+            }
+        }.start()
+    }
+
+    private fun connect(apiUrl: String): HttpURLConnection? {
+        return try {
+            val url = URL(apiUrl)
+            url.openConnection() as HttpURLConnection
+        } catch (e: MalformedURLException) {
+            throw RuntimeException("API URL이 잘못되었습니다. : $apiUrl", e)
+        } catch (e: IOException) {
+            throw RuntimeException("연결이 실패했습니다. : $apiUrl", e)
+        }
+    }
+
+    private fun readBody(body: InputStream): String {
+        val streamReader = InputStreamReader(body)
+        try {
+            BufferedReader(streamReader).use { lineReader ->
+                val responseBody = StringBuilder()
+                var line: String?
+                while (lineReader.readLine().also { line = it } != null) {
+                    responseBody.append(line)
+                }
+                return responseBody.toString()
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("API 응답을 읽는데 실패했습니다.", e)
         }
     }
 
