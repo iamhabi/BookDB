82cca0e7c4dc513be92e8b2cd50700dcd8b2b43f
iamhabi <habiam@pm.me>
2023-04-19 20:21:11 +0900

    update gitignore

---
+++ app/src/main/java/com/habidev/bookdb/DevKey.kt
@@ -0,0 +1,9 @@
+package com.habidev.bookdb
+
+class DevKey {
+    companion object {
+        const val CLIENT_ID: String = ""
+        const val CLIENT_SECRET: String = ""
+        const val URL: String = ""
+    }
+}
