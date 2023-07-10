ff2f460c3c6a0a8188fccb16040e7d4fb9c2ea90
iamhabi <habiam@pm.me>
2023-07-11 00:55:33 +0900

    performSearch onTextChanged

--- app/src/main/java/com/habidev/bookdb/fragment/SearchFragment.kt
+++ app/src/main/java/com/habidev/bookdb/fragment/SearchFragment.kt
@@ -2,6 +2,8 @@ package com.habidev.bookdb.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -104,6 +106,18 @@ class SearchFragment : Fragment() {
 
             false
         }
+
+        viewBinding.editTextSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(query: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                performSearch(query.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
     }
 
     private fun showResult(resultJson: String) {
