e888bc225f0c386af8fd4c61bc21ab30c69b6888
iamhabi <habiam@pm.me>
2023-09-30 22:32:22 +0900

    refactor BookListAdapter

--- app/src/main/java/com/habidev/bookdb/adapter/BookListAdapter.kt
+++ app/src/main/java/com/habidev/bookdb/adapter/BookListAdapter.kt
@@ -11,47 +11,63 @@ import com.habidev.bookdb.database.BookItem
 import com.habidev.bookdb.databinding.BookListItemBinding
 
 class BookListAdapter(
-    private val context: Context,
-    private var items: List<BookItem>,
-    private var onItemClickListener: OnItemClickListener
+    private val context: Context
 ): RecyclerView.Adapter<BookListAdapter.ViewHolder>() {
+    interface OnItemClickListener {
+        fun onClick(position: Int, bookItem: BookItem)
+        fun onMoreClick(position: Int, bookItem: BookItem)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    private val items: List<BookItem> = mutableListOf()
+    private var onItemClickListener: OnItemClickListener
+
+    init {
+        onItemClickListener = object : OnItemClickListener {
+            override fun onClick(position: Int, bookItem: BookItem) {
+
+            }
+
+            override fun onMoreClick(position: Int, bookItem: BookItem) {
+            }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.book_list_item, parent, false)
+        val view = LayoutInflater
+            .from(context)
+            .inflate(R.layout.book_list_item, parent, false)
 
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position])
+        val viewBinding = BookListItemBinding.bind(holder.itemView)
 
-        holder.itemView.setOnClickListener {
-            onItemClickListener.onClick(position)
+        val item = items[position]
+
+        Glide.with(viewBinding.imageViewBookCover)
+            .load(item.imageUrl)
+            .placeholder(R.drawable.book)
+            .error(R.drawable.book)
+            .into(viewBinding.imageViewBookCover)
+
+        viewBinding.textViewTitle.text = item.title
+        viewBinding.textViewAuthor.text = item.author
+
+        viewBinding.root.setOnClickListener {
+            onItemClickListener.onClick(position, item)
         }
 
-        holder.itemView.setOnLongClickListener {
-            onItemClickListener.onLongClick(position)
+        viewBinding.btnMore.setOnClickListener {
+            onItemClickListener.onMoreClick(position, item)
         }
     }
 
     override fun getItemCount() = items.size
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        private val viewBinding = BookListItemBinding.bind(itemView)
-
-        fun bind(item: BookItem) {
-            Glide.with(itemView)
-                .load(item.imageUrl)
-                .placeholder(R.drawable.book)
-                .error(R.drawable.book)
-                .into(viewBinding.imageViewBookCover)
-
-            viewBinding.textViewTitle.text = item.title
-            viewBinding.textViewAuthor.text = item.author
-        }
-    }
-
-    interface OnItemClickListener {
-        fun onClick(position: Int)
-        fun onLongClick(position: Int): Boolean
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.onItemClickListener = listener
     }
 }
