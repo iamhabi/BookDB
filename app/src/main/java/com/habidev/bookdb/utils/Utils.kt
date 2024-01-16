f96253c7576dbcf072034d91510f02056a3c3150
habi <habiam@pm.me>
2024-01-16 22:18:37 +0900

    get book info using query

--- app/src/main/java/com/habidev/bookdb/utils/Utils.kt
+++ app/src/main/java/com/habidev/bookdb/utils/Utils.kt
@@ -1,8 +1,20 @@
 package com.habidev.bookdb.utils
 
 import android.content.Context
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import com.habidev.bookdb.ApiKey
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class Utils {
     companion object {
@@ -21,6 +33,60 @@ class Utils {
             val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
             inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        interface OnGetBookInfoListener {
+            fun onGetBookInfo(result: String)
+        }
+
+        fun getBookInfo(query: String, listener: OnGetBookInfoListener) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val con = connect("${ApiKey.URL}$query")
+
+                    con?.requestMethod = "GET"
+                    con?.setRequestProperty("X-Naver-Client-Id", ApiKey.CLIENT_ID)
+                    con?.setRequestProperty("X-Naver-Client-Secret", ApiKey.CLIENT_SECRET)
+
+                    val responseCode = con!!.responseCode
+                    val result = if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                        readBody(con.inputStream)
+                    } else { // 에러 발생
+                        readBody(con.errorStream)
+                    }
+
+                    listener.onGetBookInfo(result)
+                } catch (e: Exception) {
+                    Log.e("FAIL", e.toString())
+                }
+            }
+        }
+
+        private fun connect(apiUrl: String): HttpURLConnection? {
+            return try {
+                val url = URL(apiUrl)
+                url.openConnection() as HttpURLConnection
+            } catch (e: MalformedURLException) {
+                throw RuntimeException("API URL이 잘못되었습니다. : $apiUrl", e)
+            } catch (e: IOException) {
+                throw RuntimeException("연결이 실패했습니다. : $apiUrl", e)
+            }
+        }
+
+        private fun readBody(body: InputStream): String {
+            val streamReader = InputStreamReader(body)
+            try {
+                BufferedReader(streamReader).use { lineReader ->
+                    val responseBody = StringBuilder()
+                    var line: String?
+                    while (lineReader.readLine().also { line = it } != null) {
+                        responseBody.append(line)
+                    }
+                    return responseBody.toString()
+                }
+            } catch (e: IOException) {
+                throw RuntimeException("API 응답을 읽는데 실패했습니다.", e)
+            }
         }
     }
 }
