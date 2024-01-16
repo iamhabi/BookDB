873f52e0571ee7a6984f44a9f8fd2e04a831be65
habi <habiam@pm.me>
2024-01-16 21:27:14 +0900

    ApiKey

---
+++ app/src/main/java/com/habidev/bookdb/ApiKey.kt
@@ -0,0 +1,10 @@
+package com.habidev.bookdb
+
+class ApiKey {
+    companion object {
+        const val URL = "https://openapi.naver.com/v1/search/book.json?query="
+
+        const val CLIENT_ID = ""
+        const val CLIENT_SECRET = ""
+    }
+}
