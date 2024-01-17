25c8060efd044349396bb7c25c2c27b7660e673f
habi <habiam@pm.me>
2024-01-17 19:48:35 +0900

    add group related function

--- app/src/main/java/com/habidev/bookdb/database/BookDao.kt
+++ app/src/main/java/com/habidev/bookdb/database/BookDao.kt
@@ -10,27 +10,46 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface BookDao {
     companion object {
-        const val TABLE_NAME = "books"
+        const val TABLE_NAME_BOOK = "books"
+        const val TABLE_NAME_BOOK_GROUP = "book_groups"
     }
 
     @Insert
-    suspend fun insert(bookItem: BookItem)
+    suspend fun insertBook(bookItem: BookItem)
+
+    @Insert
+    suspend fun insertGroup(groupItem: BookGroupItem)
 
     @Update
-    suspend fun update(bookItem: BookItem)
+    suspend fun updateBook(bookItem: BookItem)
+
+    @Update
+    suspend fun updateGroup(groupItem: BookGroupItem)
 
     @Delete
-    suspend fun delete(bookItem: BookItem)
+    suspend fun deleteBook(bookItem: BookItem)
 
-    @Query("DELETE FROM $TABLE_NAME")
-    suspend fun deleteAll()
+    @Delete
+    suspend fun deleteGroup(groupItem: BookGroupItem)
 
-    @Query("SELECT * FROM $TABLE_NAME WHERE isbn = :isbn")
+    @Query("DELETE FROM $TABLE_NAME_BOOK")
+    suspend fun deleteAllBooks()
+
+    @Query("DELETE FROM $TABLE_NAME_BOOK_GROUP")
+    suspend fun deleteAllGroups()
+
+    @Query("SELECT * FROM $TABLE_NAME_BOOK WHERE isbn = :isbn")
     suspend fun searchByISBN(isbn: Long): BookItem
 
-    @Query("SELECT * FROM $TABLE_NAME WHERE isbn LIKE '%' || :query || '%' OR title LIKE '%' || :query || '%' OR author LIKE '%' || :query || '%'")
-    fun search(query: String): List<BookItem>
+    @Query("SELECT * FROM $TABLE_NAME_BOOK WHERE isbn LIKE '%' || :query || '%' OR title LIKE '%' || :query || '%' OR author LIKE '%' || :query || '%'")
+    fun searchBook(query: String): List<BookItem>
 
-    @Query("SELECT * FROM $TABLE_NAME")
-    fun getAll(): Flow<List<BookItem>>
+    @Query("SELECT * FROM $TABLE_NAME_BOOK")
+    fun getBooksFlow(): Flow<List<BookItem>>
+
+    @Query("SELECT * FROM $TABLE_NAME_BOOK")
+    fun getBooksByGroup(groupItem: BookGroupItem): List<BookItem>
+
+    @Query("SELECT * FROM $TABLE_NAME_BOOK_GROUP")
+    fun getGroupsFlow(): Flow<List<BookGroupItem>>
 }
