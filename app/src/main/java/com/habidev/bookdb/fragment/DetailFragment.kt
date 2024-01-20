930e71fc0ea388edcc6b25c363898be1081eb7f2
habi <habiam@pm.me>
2024-01-21 02:07:52 +0900

    show group info and spinner listener to change group

--- app/src/main/java/com/habidev/bookdb/fragment/DetailFragment.kt
+++ app/src/main/java/com/habidev/bookdb/fragment/DetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
 import com.habidev.bookdb.R
+import com.habidev.bookdb.database.BookGroupItem
 import com.habidev.bookdb.database.BookItem
 import com.habidev.bookdb.database.BookViewModel
 import com.habidev.bookdb.databinding.DetailBinding
@@ -29,6 +31,7 @@ class DetailFragment: Fragment() {
     private lateinit var viewBinding: DetailBinding
 
     private lateinit var bookItem: BookItem
+    private lateinit var groupList: List<BookGroupItem>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +48,16 @@ class DetailFragment: Fragment() {
 
         updateInfo()
         initViewListener()
+
+        initGroupSpinner()
+        initReadStateSpinner()
+        initOwnStateSpinner()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        bookViewModel.allGroupsLiveData.removeObservers(requireActivity())
     }
 
     fun setBookItem(bookItem: BookItem) {
@@ -65,12 +78,22 @@ class DetailFragment: Fragment() {
 
         viewBinding.textViewTitle.text = bookItem.title
         viewBinding.textViewAuthor.text = bookItem.author
-        viewBinding.textViewGroup.text = bookItem.group
         viewBinding.textViewDescription.text = bookItem.description
         viewBinding.editTextComment.setText(bookItem.comment.toString(), TextView.BufferType.EDITABLE)
 
         viewBinding.spinnerReadingState.setSelection(bookItem.readingState)
         viewBinding.spinnerOwnState.setSelection(bookItem.ownState)
+
+        // TODO
+        //  set group
+
+        if (this::groupList.isInitialized) {
+            groupList.indexOfFirst {
+                it.title == bookItem.group
+            }.let { index ->
+                viewBinding.spinnerGroup.setSelection(index)
+            }
+        }
     }
 
     private fun initViewListener() {
@@ -84,62 +107,6 @@ class DetailFragment: Fragment() {
 
         viewBinding.btnClose.setOnClickListener {
             parentFragmentManager.popBackStack()
-        }
-
-        val readStateAdapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            BookItem.READ_STATE
-        )
-
-        val ownStateAdapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            BookItem.OWN_STATE
-        )
-
-        readStateAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
-        ownStateAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
-
-        viewBinding.spinnerReadingState.adapter = readStateAdapter
-        viewBinding.spinnerOwnState.adapter = ownStateAdapter
-
-        viewBinding.spinnerReadingState.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                if (this@DetailFragment::bookItem.isInitialized) {
-                    bookItem.readingState = position
-
-                    bookViewModel.updateBook(bookItem)
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-        }
-
-        viewBinding.spinnerOwnState.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                if (this@DetailFragment::bookItem.isInitialized) {
-                    bookItem.ownState = position
-
-                    bookViewModel.updateBook(bookItem)
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
         }
 
         viewBinding.editTextComment.addTextChangedListener { text ->
@@ -159,5 +126,116 @@ class DetailFragment: Fragment() {
                 Utils.hideKeyboard(requireContext(), view)
             }
         }
+
+        viewBinding.spinnerGroup.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position == 0) {
+                    // TODO
+                    //  Dialog to create new group
+                } else {
+                    if (this@DetailFragment::bookItem.isInitialized) {
+                        val group = groupList[position]
+
+                        bookItem.group = group.title
+
+                        bookViewModel.updateBook(bookItem)
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
+        viewBinding.spinnerReadingState.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (this@DetailFragment::bookItem.isInitialized) {
+                    bookItem.readingState = position
+
+                    bookViewModel.updateBook(bookItem)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
+        viewBinding.spinnerOwnState.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (this@DetailFragment::bookItem.isInitialized) {
+                    bookItem.ownState = position
+
+                    bookViewModel.updateBook(bookItem)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+    }
+
+    private fun initGroupSpinner() {
+        bookViewModel.allGroupsLiveData.observe(requireActivity()) { groupList ->
+            this.groupList = groupList
+
+            val groupArray = arrayListOf<String>()
+
+            for (group in groupList) {
+                groupArray.add(group.title)
+            }
+
+            groupArray.add("Create new group")
+
+            val groupAdapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                groupArray
+            )
+
+            groupAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
+
+            viewBinding.spinnerGroup.adapter = groupAdapter
+        }
+    }
+
+    private fun initReadStateSpinner() {
+        val readStateAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            BookItem.READ_STATE
+        )
+
+        readStateAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
+
+        viewBinding.spinnerReadingState.adapter = readStateAdapter
+    }
+
+    private fun initOwnStateSpinner() {
+        val ownStateAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            BookItem.OWN_STATE
+        )
+
+        ownStateAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
+
+        viewBinding.spinnerOwnState.adapter = ownStateAdapter
     }
 }
