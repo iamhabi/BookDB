4849eaeaf0dc04487f11ba34e97e1864361d4367
iamhabi <habiam@pm.me>
2024-01-23 22:56:16 +0900

    Toast create group message by insert success state

--- app/src/main/java/com/habidev/bookdb/fragment/GroupListFragment.kt
+++ app/src/main/java/com/habidev/bookdb/fragment/GroupListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,6 +14,9 @@ import com.habidev.bookdb.database.BookGroupItem
 import com.habidev.bookdb.database.BookViewModel
 import com.habidev.bookdb.databinding.GroupListBinding
 import com.habidev.bookdb.utils.Utils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class GroupListFragment: Fragment() {
     companion object {
@@ -106,7 +110,19 @@ class GroupListFragment: Fragment() {
             } else {
                 viewBinding.editTextAddGroup.text.clear()
 
-                bookViewModel.insertGroup(BookGroupItem(0, text))
+                CoroutineScope(Dispatchers.IO).launch {
+                    val isInsertSuccess = bookViewModel.insertGroup(BookGroupItem(0, text))
+
+                    val message = if (isInsertSuccess) {
+                        "Create successful"
+                    } else {
+                        "Already exist"
+                    }
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
 
