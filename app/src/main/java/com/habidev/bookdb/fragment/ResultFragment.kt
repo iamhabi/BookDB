04722550f73dc47d8fd70bfe27add13455775b5d
iamhabi <habiam@pm.me>
2024-01-23 22:53:29 +0900

    Toast by insert success state

--- app/src/main/java/com/habidev/bookdb/fragment/ResultFragment.kt
+++ app/src/main/java/com/habidev/bookdb/fragment/ResultFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
@@ -113,7 +114,19 @@ class ResultFragment: Fragment() {
 
         viewBinding.btnAddBookmark.setOnClickListener {
             if (this::bookItem.isInitialized) {
-                bookViewModel.insertBook(bookItem)
+                CoroutineScope(Dispatchers.IO).launch {
+                    val isInsertSuccess = bookViewModel.insertBook(bookItem)
+
+                    val message = if (isInsertSuccess) {
+                        "Add successful"
+                    } else {
+                        "Already on database"
+                    }
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
 
