c22bcb99dd7c6bc7d2119c5f0cf0fb4d00ac54d7
iamhabi <habiam@pm.me>
2024-01-25 23:19:09 +0900

    check/request camera permission

--- app/src/main/java/com/habidev/bookdb/utils/Utils.kt
+++ app/src/main/java/com/habidev/bookdb/utils/Utils.kt
@@ -1,9 +1,14 @@
 package com.habidev.bookdb.utils
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.habidev.bookdb.ApiKey
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,6 +24,24 @@ import java.net.URL
 class Utils {
     companion object {
         const val TAG = "BookDB"
+
+        private const val PERMISSION_CAMERA = Manifest.permission.CAMERA
+        const val PERMISSION_CAMERA_REQUEST_CODE = 50234
+
+        fun isCamPermissionGranted(context: Context): Boolean {
+            return ContextCompat.checkSelfPermission(
+                context,
+                PERMISSION_CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+        fun requestCameraPermission(activity: Activity) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(PERMISSION_CAMERA),
+                PERMISSION_CAMERA_REQUEST_CODE
+            )
+        }
 
         fun showKeyboard(context: Context, view: View) {
             val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
