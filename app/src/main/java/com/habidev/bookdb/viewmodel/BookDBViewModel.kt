663429f3ed3dfb364e25bc5f7b7a4b357f103a2e
iamhabi <iamhabi@pm.me>
2024-04-03 10:05:12 +0900

    get groups on init ViewModel

--- app/src/main/java/com/habidev/bookdb/viewmodel/BookDBViewModel.kt
+++ app/src/main/java/com/habidev/bookdb/viewmodel/BookDBViewModel.kt
@@ -1,10 +1,12 @@
 package com.habidev.bookdb.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.habidev.bookdb.api.BookDBClient
 import com.habidev.bookdb.data.BookItem
 import com.habidev.bookdb.data.GroupBookItem
 import com.habidev.bookdb.data.GroupItem
@@ -12,7 +14,21 @@ import com.habidev.bookdb.database.BookRepository
 import kotlinx.coroutines.launch
 
 class BookDBViewModel(private val repository: BookRepository): ViewModel() {
-    fun init() {}
+    fun init() {
+        BookDBClient.getList { books ->
+            _allBooks.value = books as MutableList<BookItem>
+        }
+
+        BookDBClient.getGroups { groups ->
+            _groups.value = groups as MutableList<GroupItem>
+        }
+    }
+
+    private val _allBooks: MutableLiveData<MutableList<BookItem>> = MutableLiveData(mutableListOf())
+    private val _groups: MutableLiveData<MutableList<GroupItem>> = MutableLiveData(mutableListOf())
+
+    val allBooks: LiveData<MutableList<BookItem>> = _allBooks
+    val groups: LiveData<MutableList<GroupItem>> = _groups
 
     val allBooksLiveData: LiveData<List<BookItem>> = repository.allBooksFlow.asLiveData()
     val allGroupsLiveData: LiveData<List<GroupItem>> = repository.allGroupsFlow.asLiveData()
